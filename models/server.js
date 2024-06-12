cdc9dad493bc011211855471732007d8e7f2cbf7
Renzo Porta <renzo.porta@unmsm.edu.pe>
2024-06-12 15:38:25 -0500

    Primer Commit

---
+++ models/server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const cors = require('cors')
+class Server {
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+        this.usuariosPath='/api/usuarios';
+        //Middelware
+        this.middelwares();
+        //Rutas de la aplicación
+        this.routes();
+    }
+
+    //A que se define el metodo middleware que publicara la carpeta public
+    middelwares() {
+        //CORS
+        this.app.use(cors());
+        //Lectura y parseo del body recibe lo que se envia
+        this.app.use(express.json());
+        //directorio publico
+        this.app.use(express.static('public'));
+    }
+
+    routes() {
+      this.app.use(this.usuariosPath,require('../routes/usuarios'))
+    }
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log('Servidor corriendo en puerto ', this.port)
+        })
+    }
+}
+module.exports = Server;
